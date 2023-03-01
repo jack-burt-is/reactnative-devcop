f6f188487c74d19d5c5ec1e64d7bc7488de4a067
Jack Burt <burtj@informed.co.uk>
2023-03-01 12:48:40 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import * as GovUK from 'govuk-react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GovUK.GlobalStyle />
+      <GovUK.TopNav
+        serviceTitle={
+          <GovUK.TopNav.Anchor>
+            React Native GDS Application
+          </GovUK.TopNav.Anchor>
+        }
+        search={
+          <GovUK.SearchBox>
+            <GovUK.SearchBox.Input placeholder="Search GOV.UK" />
+            <GovUK.SearchBox.Button />
+          </GovUK.SearchBox>
+        }
+      >
+        <GovUK.TopNav.NavLink>
+          Home
+        </GovUK.TopNav.NavLink>
+        <GovUK.TopNav.NavLink>
+          Forms
+        </GovUK.TopNav.NavLink>
+      </GovUK.TopNav>
+      <GovUK.Page.WidthContainer>
+        <GovUK.Page.Main>
+          <GovUK.H2>Welcome to the govuk-react example application.</GovUK.H2>
+          <GovUK.Paragraph>Click Start to continue.</GovUK.Paragraph>
+          <GovUK.Button>
+            Start now
+          </GovUK.Button>
+        </GovUK.Page.Main>
+      </GovUK.Page.WidthContainer>
+      <GovUK.Footer />
     </View>
   );
 }
@@ -13,8 +44,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
